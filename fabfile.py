3d308989ab014d024d51c9e58f36a9620ae56d6f
Antkk10 <antoniomfc90@gmail.com>
2017-01-29 13:55:13 +0100

    Archivo de configuración del supervisor y actualización de playbock de ansible para que instale el supervisor en azure.Funciones de fabric actualizadas y añadida la función StopApp. close #27

---
+++ fabfile.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+from fabric.api import sudo, cd, env, run, shell_env
+import os
+
+
+def InstalaAplicacion():
+    """ Función para descargar el bot del repositorio. """
+
+    # Descargamos la app
+    run('git clone https://github.com/Antkk10/BotTelegramInfoActividadesUGR.git')
+
+    # Obtenemos las variables de entorno
+
+    # Accedemos y terminamos de instalar los requisitos
+    run('cd BotTelegramInfoActividadesUGR/ && pip install -r requirements.txt')
+
+def EjecutarApp():
+    """ Función para ejecutar el Bot. """
+    with shell_env(HOST_BD=os.environ['HOST_BD'],
+                    USER_BD=os.environ['USER_BD'],
+                    PW_BD=os.environ['PW_BD'],
+                    NAME_BD=os.environ['NAME_BD'],
+                    TOKENBOT=os.environ['TOKENBOT']
+                    ):
+        run('sudo supervisorctl start botactividades')
+
+def StopApp():
+    """ Función que para el bot. """
+    run('sudo supervisorctl stop botactividades')
